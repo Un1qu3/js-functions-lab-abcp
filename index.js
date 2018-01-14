1cec8cd519ca051a0ec24aac5f9a448cc5097a0b
HARJEET SINGH <harrysbhatti22@gmail.com>
2018-01-14 11:57:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // write your code below!
+function happyHolidays(){return "Happy holidays!"}
+function happyHolidaysT(name){return `Happy holidays, ${name}`}
+function happyHolidayTo(name,holiday){return `Happy ${holiday}, ${name}`}
