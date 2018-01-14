bb9c72a45a8aa7370760d581d0c4a194f52bbed3
HARJEET SINGH <harrysbhatti22@gmail.com>
2018-01-14 11:59:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // write your code below!
+var name = "Harjeet";
+var holiday = "Republic Day";
+var days = 12;
+
 function happyHolidays(){return "Happy holidays!"}
 function happyHolidaysT(name){return `Happy holidays, ${name}`}
 function happyHolidayTo(name,holiday){return `Happy ${holiday}, ${name}`}
