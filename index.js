828ab01e78ae9e1656ab1a053a5e3976dea96a6e
HARJEET SINGH <harrysbhatti22@gmail.com>
2018-01-14 12:00:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var holiday = "Mother's Day";
 var days = 20;
 
 function happyHolidays(){return "Happy holidays!"}
-function happyHolidaysT(name){return `Happy holidays, ${name}`}
-function happyHolidayTo(name,holiday){return `Happy ${holiday}, ${name}`}
-function holidayCountdown(holiday,days){return `It's ${days} days until ${holiday}`}
+function happyHolidaysT(name){return `Happy holidays, ${name}!`}
+function happyHolidayTo(name,holiday){return `Happy ${holiday}, ${name}!`}
+function holidayCountdown(holiday,days){return `It's ${days} days until ${holiday}!`}
