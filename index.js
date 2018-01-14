b068eb648758a1013bd549dbee3264ebccf5ea4d
HARJEET SINGH <harrysbhatti22@gmail.com>
2018-01-14 12:04:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var days = 20;
 
 function happyHolidays(){return "Happy holidays!"}
 function happyHolidaysTo(name){return `Happy holidays, ${name}!`}
-function happyHolidayTo(name,holiday){return `Happy ${holiday}, ${name}!`}
+function happyHolidayTo(name,holiday){return `Happy ${holiday} Day, ${name}!`}
 function holidayCountdown(holiday,days){return `It's ${days} days until ${holiday}!`}
